628055bf2f80ad6dd0c55725dabc409e80a0a7c1
SiddiquiMonis401 <monisms15@gmail.com>
2023-10-01 00:49:20 +0500

    configured theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import  { useState } from "react";
+import { ThemeProvider } from "@emotion/react";
+import {
+  Card,
+  CardContent,
+  Switch,
+  Typography,
+  createTheme,
+} from "@mui/material";
 
 function App() {
+  const [darkMode, setDarkMode] = useState<boolean>(false);
+  const theme = createTheme({
+    palette: {
+      mode: darkMode ? "dark" : "light",
+    },
+  });
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Switch onChange={() => setDarkMode((prev) => !prev)} />
+      <Card >
+        <CardContent>
+          <Typography color="text.secondary" gutterBottom>
+            Testing theme
+          </Typography>
+        </CardContent>
+      </Card>
+    </ThemeProvider>
   );
 }
 
